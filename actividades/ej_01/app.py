8f954c1f13164bc8eb6c815e3f85254c30143838
kicklauti <101152011+kicklauti@users.noreply.github.com>
2025-04-25 07:45:14 -0300

    Update app.py

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,22 @@
-# Aplicacion del servidor
+from microdot import Microdot
+from microdot import send_file
+
+app = Microdot()
+
+@app.get('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.get('/styles/base.css')
+async def index(request):
+    return send_file('styles/base.css')
+
+@app.get('/scripts/base.js')
+async def index(request):
+    return send_file('scripts/base.js')
+
+@app.get('/')
+async def image(request):
+    return send_file('/static/image.jpg', max_age=3600)  # in seconds
+
+app.run()# Aplicacion del servidor
